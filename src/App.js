032e79f21a4dc1e7291280b00f41470fb06727c2
Daniel Trone <danieljtrone@gmail.com>
2019-09-02 09:02:57 -0500

    string replace

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import reactStringReplace from "react-string-replace";
 
-function App() {
+function App({data}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map(generateTweet)}
     </div>
   );
 }
 
 export default App;
+
+
+function generateTweet(tweet) {
+
+  const linkedTweet = replaceLinks(tweet);
+
+  return <div className="card container"><p className="container">
+  <span className="bold">@{tweet.user.screen_name}&nbsp;&nbsp;</span>
+  {linkedTweet}
+  </p></div>;
+}
+
+
+function replaceLinks(tweet) {
+
+  const tweet_copy = JSON.parse(JSON.stringify(tweet));
+
+  let full_text = tweet_copy.full_text;
+
+  const key = {};
+  const media_key = {};
+
+  // create a text replace system using url indices
+  tweet.entities.urls.forEach(url => {
+    const text = Array.from(tweet.full_text).slice(url.indices[0], url.indices[1]).join('');
+    key[text] = { expanded: url.expanded_url, display: url.display_url };
+  });
+
+  tweet.entities.media && tweet.entities.media.forEach(item => {
+    const text = Array.from(tweet.full_text).slice(item.indices[0], item.indices[1]).join('');
+    media_key[text] = { media_url: item.media_url_https, type: item.type };
+  });
+
+  Object.keys(key).forEach(k => {
+    // full_text = full_text.replace(k, <a href={key[k].expanded} target="_blank">${key[k].display}</a>);
+    full_text = reactStringReplace(full_text, k, (match, i) => (
+      <a href={key[k].expanded} target="_blank">{key[k].display}</a>
+    ));
+  });
+
+  Object.keys(media_key).forEach(k => {
+
+    const media_url_array = media_key[k].media_url.split(".");
+    const extension = media_url_array.pop();
+    const ext_query = extension === "jpeg" || extension === "jpg" ? "jpg" : "png";
+    const removed_type = media_url_array.join(".");
+
+    const append_querystring = `${removed_type}?format=${ext_query}&name=small`;
+    const append_querystring_lg = `${removed_type}?format=${ext_query}&name=large`;
+
+    full_text = reactStringReplace(full_text, k, (match, i) => (
+        <React.Fragment><br />
+          <div className="image-container">
+            <a href={append_querystring_lg} target="_blank">
+              <img src={append_querystring} alt={"Embedded " + media_key[k].type} />
+            </a>
+          </div>
+        </React.Fragment>
+    ));
+
+  });
+
+  return full_text;
+
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppContainer from './AppContainer';
 import {runAuthentication} from "./Service/authentication";
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
@@ -43,7 +43,7 @@ runAuthentication()
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AppContainer />
   </Provider>,
   document.getElementById('root')
 );
@@ -54,63 +54,3 @@ ReactDOM.render(
 serviceWorker.unregister();
 
 
-function generateTweet(tweet) {
-
-  const linkedTweet = replaceLinks(tweet);
-
-  return `<div class="card container"><p class="container">
-  
-  <span class="bold">@${linkedTweet.user.screen_name}</span>
-  
-  ${linkedTweet.full_text}
-  
-  
-  </p></div>
-  
-  
-  `;
-}
-
-function replaceLinks(tweet) {
-
-  const tweet_copy = JSON.parse(JSON.stringify(tweet));
-
-  // console.log(JSON.stringify(tweet))
-
-  let full_text = tweet_copy.full_text;
-
-  const key = {};
-  const media_key = {};
-
-  // create a text replace system using url indices
-  tweet.entities.urls.forEach(url => {
-    const text = Array.from(tweet.full_text).slice(url.indices[0], url.indices[1]).join('');
-    key[text] = { expanded: url.expanded_url, display: url.display_url };
-  });
-
-  tweet.entities.media && tweet.entities.media.forEach(item => {
-    const text = Array.from(tweet.full_text).slice(item.indices[0], item.indices[1]).join('');
-    media_key[text] = { media_url: item.media_url_https, type: item.type };
-  });
-
-  Object.keys(key).forEach(k => {
-    full_text = full_text.replace(k, `<a href="${key[k].expanded}" target="_blank">${key[k].display}</a>`);
-  });
-
-  Object.keys(media_key).forEach(k => {
-
-    const media_url_array = media_key[k].media_url.split(".");
-    const extension = media_url_array.pop();
-    const ext_query = extension === "jpeg" || extension === "jpg" ? "jpg" : "png";
-    const removed_type = media_url_array.join(".");
-
-    const append_querystring = `${removed_type}?format=${ext_query}&name=small`;
-    const append_querystring_lg = `${removed_type}?format=${ext_query}&name=large`;
-
-    full_text = full_text.replace(k, `<br /><div class="image-container"><a href="${append_querystring_lg}" target="_blank"><img src="${append_querystring}" alt="Embedded ${media_key[k].type}" /></a></div>`);
-  });
-
-  tweet_copy.full_text = full_text;
-
-  return tweet_copy;
-}
