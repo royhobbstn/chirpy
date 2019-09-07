adaf065bfa748b6a213e4e3c56126940b1c68847
Daniel Trone <danieljtrone@gmail.com>
2019-09-07 17:47:05 -0500

    Updates

---
+++ precache-manifest.6a8d1a1f5a40523d7828df7a8985f8ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64482e479dabf73c3c596392e5bbbc06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77ea50605a84b32bbeb7",
+    "url": "/static/css/main.665836f7.chunk.css"
+  },
+  {
+    "revision": "cd02f119dca0d40d0f51",
+    "url": "/static/js/2.0eae0313.chunk.js"
+  },
+  {
+    "revision": "77ea50605a84b32bbeb7",
+    "url": "/static/js/main.61c3c42d.chunk.js"
+  },
+  {
+    "revision": "60fb41f4108b134b9064",
+    "url": "/static/js/runtime~main.7eef82d6.js"
+  }
+]);
